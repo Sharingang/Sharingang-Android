24717d3120626e535bc7854806a7e2d097ecc465
Lúcás Meier <cronokirby@gmail.com>
2021-03-06 14:37:47 +0100

    Add basic form connected to main screen
    Now you can enter a description, and then see that item description when you go back to the main screen

--- app/src/androidTest/java/com/example/sharingang/NewItemActivityTest.kt
+++ app/src/androidTest/java/com/example/sharingang/NewItemActivityTest.kt
@@ -1,7 +1,10 @@
 package com.example.sharingang
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -15,8 +18,25 @@ class NewItemActivityTest {
     @get:Rule
     val activityRule = ActivityScenarioRule(NewItemActivity::class.java)
 
+    private val expectedString = "Hello World!"
+
     @Test
     fun newItemPromptIsVisible() {
         onView(withId(R.id.newItemPrompt)).check(matches(withText("New Item")))
+    }
+
+    @Test
+    fun aDescriptionCanBeEnteredAndSeenOnMainActivity() {
+        Intents.init()
+        onView(withId(R.id.editItemDescription)).perform(
+            typeText(expectedString),
+            closeSoftKeyboard()
+        )
+        val button = onView(withId(R.id.createItemButton))
+        button.check(matches(withText("Create Item")))
+        button.perform(click())
+        Intents.intended(IntentMatchers.hasComponent(MainActivity::class.qualifiedName))
+        onView(withId(R.id.lastDescription)).check(matches(withText(expectedString)))
+        Intents.release()
     }
 }
--- app/src/main/java/com/example/sharingang/MainActivity.kt
+++ app/src/main/java/com/example/sharingang/MainActivity.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.sharingang.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+       binding.description = intent.getStringExtra("LAST_DESCRIPTION")
     }
 
     fun createNewItem(view: View) {
