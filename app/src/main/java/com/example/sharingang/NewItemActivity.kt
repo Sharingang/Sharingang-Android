d947437c2e0f386ed84855df322644be34763bd1
Lúcás Meier <cronokirby@gmail.com>
2021-03-06 14:37:47 +0100

    Add basic form connected to main screen
    Now you can enter a description, and then see that item description when you go back to the main screen

--- app/src/main/java/com/example/sharingang/NewItemActivity.kt
+++ app/src/main/java/com/example/sharingang/NewItemActivity.kt
@@ -1,11 +1,26 @@
 package com.example.sharingang
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.sharingang.databinding.ActivityNewItemBinding
 
 class NewItemActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNewItemBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_item)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_new_item)
+    }
+
+    fun createNewItem(view: View) {
+        val intent = Intent(baseContext, MainActivity::class.java)
+        intent.putExtra(
+            "LAST_DESCRIPTION",
+            binding.description
+        )
+        startActivity(intent)
     }
 }
