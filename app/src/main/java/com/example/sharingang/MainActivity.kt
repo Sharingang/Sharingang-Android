af95855cf1202583189e735e37f3a78ec442ae65
Lúcás Meier <cronokirby@gmail.com>
2021-03-06 18:03:28 +0100

    Launch NewItemActivity just for the result
    This way we don't create a dangling activity, but instead just launch and then terminate the item creation activity.

--- app/src/main/java/com/example/sharingang/MainActivity.kt
+++ app/src/main/java/com/example/sharingang/MainActivity.kt
@@ -1,22 +1,32 @@
 package com.example.sharingang
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.sharingang.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val resultLauncher =
+        registerForActivityResult(StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                binding.description = result.data?.getStringExtra(Intent.EXTRA_TEXT) ?: ""
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.description = intent.getStringExtra(Intent.EXTRA_TEXT)
     }
 
     fun createNewItem(view: View) {
         val intent = Intent(this, NewItemActivity::class.java)
-        startActivity(intent)
+        resultLauncher.launch(intent)
     }
 }
