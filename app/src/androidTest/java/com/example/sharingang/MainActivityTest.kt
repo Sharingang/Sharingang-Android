1859447985aa9582985b85cee1f1eba348b94321
Lúcás Meier <cronokirby@gmail.com>
2021-03-06 13:03:23 +0100

    Add an activity to hold the item creation form
    This activity can be launched from the Main Activity, by clicking a button.

---
+++ app/src/androidTest/java/com/example/sharingang/MainActivityTest.kt
@@ -0,0 +1,32 @@
+package com.example.sharingang
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun clickingOnButtonStartsNewItemActivity() {
+        Intents.init()
+        val button = onView(withId(R.id.newItemButton))
+        button.check(matches(withText("New Item")))
+        button.perform(click())
+        intended(hasComponent(NewItemActivity::class.qualifiedName))
+        Intents.release()
+    }
+}
