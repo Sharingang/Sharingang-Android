0013c3ba4e417b213049c570fa15ce110413a94c
Lúcás Meier <cronokirby@gmail.com>
2021-03-06 13:03:23 +0100

    Add an activity to hold the item creation form
    This activity can be launched from the Main Activity, by clicking a button.

--- app/src/main/java/com/example/sharingang/MainActivity.kt
+++ app/src/main/java/com/example/sharingang/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.sharingang
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun createNewItem(view: View) {
+        val intent = Intent(this, NewItemActivity::class.java)
+        startActivity(intent)
     }
 }
