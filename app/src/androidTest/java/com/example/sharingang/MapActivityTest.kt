ba0b2c7ebe6cd643920269f06c2bfca5b8587601
Sebastian Velez de Villa <sebastian.velezdevilla@epfl.ch>
2021-03-15 10:33:10 +0100

    Clean imports

--- app/src/androidTest/java/com/example/sharingang/MapActivityTest.kt
+++ app/src/androidTest/java/com/example/sharingang/MapActivityTest.kt
@@ -2,14 +2,12 @@ package com.example.sharingang
 
 import android.os.Build
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.rule.GrantPermissionRule
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.UiSelector
 import org.hamcrest.core.StringContains.containsString
@@ -27,14 +25,16 @@ class MapActivityTest {
     private fun grantPermission() {
         val instrumentation = InstrumentationRegistry.getInstrumentation()
         if (Build.VERSION.SDK_INT >= 23) {
-            val allowPermission = UiDevice.getInstance(instrumentation).findObject(UiSelector().text(
+            val allowPermission = UiDevice.getInstance(instrumentation).findObject(
+                UiSelector().text(
                     when {
                         Build.VERSION.SDK_INT == 23 -> "Allow"
                         Build.VERSION.SDK_INT <= 28 -> "ALLOW"
                         Build.VERSION.SDK_INT == 29 -> "Allow only while using the app"
                         else -> "While using the app"
                     }
-            ))
+                )
+            )
             if (allowPermission.exists()) {
                 allowPermission.click()
             }
