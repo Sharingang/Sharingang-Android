26dc729916fe2cdb07da7a93d93836354b9d2da7
Lúcás Meier <cronokirby@gmail.com>
2021-03-22 18:02:20 +0100

    Integrate Room database
    Now we store items to a database on refresh, so things are locally cached

--- app/src/main/java/com/example/sharingang/items/ItemRepositoryModule.kt
+++ app/src/main/java/com/example/sharingang/items/ItemRepositoryModule.kt
@@ -1,20 +1,42 @@
 package com.example.sharingang.items
 
+import android.content.Context
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 /**
  * Tells Hilt to use an instance of FirestoreItemStore when we require an ItemStore
  */
 @Module
-@InstallIn(ViewModelComponent::class)
+@InstallIn(SingletonComponent::class)
 object ItemRepositoryModule {
 
+    @Singleton
     @Provides
-    fun provideItemRepository(): ItemRepository {
-        return CachedItemRepository(FirestoreItemStore())
+    fun provideItemStore(): ItemStore {
+        return FirestoreItemStore()
+    }
+
+    @Singleton
+    @Provides
+    fun provideItemRepository(repo: CachedItemRepository): ItemRepository {
+        return repo
+    }
+
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext app: Context): ItemDatabase {
+        return Room.databaseBuilder(app, ItemDatabase::class.java, "item_database").build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideItemDao(db: ItemDatabase): ItemDao {
+        return db.itemDao
     }
 }
