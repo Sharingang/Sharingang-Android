fd8d877c3907825a0a2059fcfa26bb3bf9813f4a
lumapools <benedek.hauer@gmail.com>
2021-03-23 04:51:26 +0100

    Add login status and logout button (useless yet)
    User can see if they are logged in or not. The logout button does not do anything yet.

--- app/src/main/java/com/example/sharingang/ItemsListFragment.kt
+++ app/src/main/java/com/example/sharingang/ItemsListFragment.kt
@@ -2,9 +2,11 @@ package com.example.sharingang
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -15,10 +17,19 @@ import com.example.sharingang.items.Item
 import com.example.sharingang.items.ItemListener
 import com.example.sharingang.items.ItemsAdapter
 import com.example.sharingang.items.ItemsViewModel
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 
 class ItemsListFragment : Fragment() {
 
     private val viewModel: ItemsViewModel by activityViewModels()
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    lateinit var loginStatus: TextView
+    private val RC_SIGN_IN = 9001
 
     private fun setupNavigation() {
         viewModel.navigateToEditItem.observe(viewLifecycleOwner, { item ->
@@ -46,6 +57,7 @@ class ItemsListFragment : Fragment() {
         val binding: FragmentItemsListBinding =
                 DataBindingUtil.inflate(inflater, R.layout.fragment_items_list, container, false)
         binding.viewModel = viewModel
+        loginStatus = binding.loginStatus
         binding.newItemButton.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_itemsListFragment_to_newItemFragment)
         }
@@ -60,11 +72,78 @@ class ItemsListFragment : Fragment() {
             startActivity(Intent(this.activity, MapActivity::class.java))
         }
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("771023799063-kmve17s9cfu6ckd3kijcv8vdvvdqb58s.apps.googleusercontent.com")
+            .requestServerAuthCode("771023799063-kmve17s9cfu6ckd3kijcv8vdvvdqb58s.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = activity?.let { GoogleSignIn.getClient(it, gso) }!!
+
+        binding.loginButton.setOnClickListener {
+            signIn()
+        }
+
         return binding.root
     }
 
     fun gotoSearchPage(view : View){
         view.findNavController().navigate(R.id.action_itemsListFragment_to_searchFragment5)
     }
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(
+            signInIntent, RC_SIGN_IN
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task =
+                GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(
+                ApiException::class.java
+            )
+            loginStatus.text = "Logged in as ${account.displayName}"
+            // Signed in successfully
+            val googleId = account?.id ?: ""
+            Log.i("Google ID",googleId)
+
+            val googleFirstName = account?.givenName ?: ""
+            Log.i("Google First Name", googleFirstName)
+
+            val googleLastName = account?.familyName ?: ""
+            Log.i("Google Last Name", googleLastName)
+
+            val googleEmail = account?.email ?: ""
+            Log.i("Google Email", googleEmail)
+
+            val googleProfilePicURL = account?.photoUrl.toString()
+            Log.i("Google Profile Pic URL", googleProfilePicURL)
+
+            val googleIdToken = account?.idToken ?: ""
+            Log.i("Google ID Token", googleIdToken)
+
+        } catch (e: ApiException) {
+            // Sign in was unsuccessful
+            Log.e(
+                "failed code=", e.statusCode.toString()
+            )
+        }
+    }
+
 }
+
+
+
+
+
 
