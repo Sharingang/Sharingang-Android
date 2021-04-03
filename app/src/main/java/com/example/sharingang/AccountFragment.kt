0e62d0ea0b25ad535332fb4401d08ea4a0556f72
lumapools <benedek.hauer@gmail.com>
2021-04-03 03:19:39 +0200

    Add persistent login information and status
    The user can now persistently be logged in even if they completely close the app. They can log out at any moment if they want to. They will not need to enter their credentials each time they open the app.

--- app/src/main/java/com/example/sharingang/AccountFragment.kt
+++ app/src/main/java/com/example/sharingang/AccountFragment.kt
@@ -1,7 +1,10 @@
 package com.example.sharingang
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,20 +15,30 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.sharingang.databinding.FragmentAccountBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.OnCompleteListener
 
 
 class AccountFragment : Fragment() {
     var resultLauncher: ActivityResultLauncher<Intent>? = null
+    lateinit var sharedPreferences: SharedPreferences
+    lateinit var editor: SharedPreferences.Editor
+
 
     private enum class AccountStatus {
         LOGGED_IN,
         LOGGED_OUT
     }
 
+    private enum class LoginErrorCode {
+        SUCCESS,
+        FAILURE,
+        SIGNOUT
+    }
+
+    @SuppressLint("CommitPrefEdits")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,38 +49,53 @@ class AccountFragment : Fragment() {
             container,
             false
         )
-        binding.logoutButton.visibility = View.GONE
+
+        sharedPreferences = activity?.getSharedPreferences(
+            getString(R.string.preference_user_info),
+            Context.MODE_PRIVATE
+        )!!
+        editor = sharedPreferences.edit()
+        editor.apply()
+
+        restorePreferences(binding)
         createLauncher(binding)
         signInSetup(binding)
         return binding.root
     }
 
     private fun createLauncher(binding: FragmentAccountBinding) {
+
         resultLauncher =
             registerForActivityResult(
-                ActivityResultContracts.
-                StartActivityForResult()) { result -> if (result.resultCode == Activity.RESULT_OK) {
+                ActivityResultContracts.StartActivityForResult()
+            ) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
                     try {
-                        val account = GoogleSignIn.getSignedInAccountFromIntent(result.data).getResult(ApiException::class.java)
-                        "Status: Logged in as \n${account.displayName}\n(${account.email})".also { binding.accountStatus.text = it }
+                        val account = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                            .getResult(ApiException::class.java)
+                        "Status: Logged in as \n${account.displayName}\n".also { binding.accountStatus.text = it }
+                        updateAccountPreferences(editor, LoginErrorCode.SUCCESS, account)
                         updateUI(accountStatus = AccountStatus.LOGGED_IN, binding)
                     } catch (e: ApiException) {
                         updateUI(accountStatus = AccountStatus.LOGGED_OUT, binding)
                         binding.accountStatus.text = getString(R.string.failed_login_message)
+                        updateAccountPreferences(editor, LoginErrorCode.FAILURE, null)
                     }
                 } else {
                     updateUI(accountStatus = AccountStatus.LOGGED_OUT, binding)
+                    updateAccountPreferences(editor, LoginErrorCode.FAILURE, null)
                 }
             }
+        editor.apply()
     }
 
     private fun signInSetup(binding: FragmentAccountBinding) {
         val clientId=
-            activity?.resources?.getString(R.string.default_web_client_id);
+            activity?.resources?.getString(R.string.default_web_client_id)
         val gso =
             GoogleSignInOptions
             .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(clientId)
+            .requestIdToken(clientId!!)
             .requestServerAuthCode(clientId)
             .requestEmail()
             .build()
@@ -77,16 +105,25 @@ class AccountFragment : Fragment() {
         binding.loginButton.setOnClickListener {
             sign_in(googleSignInClient, binding)
         }
-        binding.logoutButton.setOnClickListener() { sign_out(signInClient = googleSignInClient, binding) }
+        binding.logoutButton.setOnClickListener() { sign_out(
+            signInClient = googleSignInClient, binding)
+        }
+
     }
 
     private fun sign_in(signInClient: GoogleSignInClient, binding: FragmentAccountBinding) {
         val toLaunch: Intent = signInClient.signInIntent
         resultLauncher?.launch(toLaunch)
-        updateUI(accountStatus = AccountStatus.LOGGED_IN, binding) }
+        updateUI(accountStatus = AccountStatus.LOGGED_IN, binding)
+
+    }
 
     private fun sign_out(signInClient: GoogleSignInClient, binding: FragmentAccountBinding) {
-        signInClient.signOut().addOnCompleteListener(requireActivity()) { updateUI(accountStatus = AccountStatus.LOGGED_OUT, binding) } }
+        signInClient.signOut().addOnCompleteListener(requireActivity()) { updateUI(
+            accountStatus = AccountStatus.LOGGED_OUT, binding)
+            updateAccountPreferences(editor, LoginErrorCode.SIGNOUT, null)
+        }
+    }
 
     private fun updateUI(accountStatus: AccountStatus, binding: FragmentAccountBinding) {
         if(accountStatus == AccountStatus.LOGGED_IN) {
@@ -96,5 +133,50 @@ class AccountFragment : Fragment() {
             binding.logoutButton.visibility = View.GONE
             binding.accountStatus.text = getString(R.string.text_logged_out)
         }
+    }
+
+    private fun restorePreferences(binding: FragmentAccountBinding) {
+        val sharedPreferences = context?.getSharedPreferences(
+            getString(R.string.preference_user_info),
+            Context.MODE_PRIVATE
+        )
+        if (sharedPreferences != null) {
+            if(sharedPreferences.getString(getString(R.string.key_account_name), "").equals("")) {
+                updateUI(AccountStatus.LOGGED_OUT, binding)
+            }
+            else {
+                "Status: Logged in as \n${sharedPreferences.getString(
+                    getString(R.string.key_account_name), "")}"
+                    .also { binding.accountStatus.text = it }
+                updateUI(AccountStatus.LOGGED_IN, binding)
+            }
+        }
+        else {
+            updateUI(AccountStatus.LOGGED_OUT, binding)
+        }
+    }
+
+    private fun updateAccountPreferences(
+        editor: SharedPreferences.Editor, code: LoginErrorCode,
+        account: GoogleSignInAccount?
+    ) {
+        if(code == LoginErrorCode.FAILURE || code == LoginErrorCode.SIGNOUT) {
+            editor.putString(getString(R.string.key_account_uid), "")
+            editor.putString(getString(R.string.key_account_name), "")
+            editor.putString(getString(R.string.key_account_picture), "")
+            editor.putString(getString(R.string.key_account_email), "")
+        }
+        else {
+            editor.putString(getString(R.string.key_account_uid), account?.id)
+            editor.putString(getString(R.string.key_account_name), account?.displayName)
+            editor.putString(
+                getString(R.string.key_account_picture),
+                account?.photoUrl!!.toString()
+            )
+            editor.putString(getString(R.string.key_account_email),
+                account.email!!.toString()
+            )
+        }
+        editor.apply()
     }
 }
