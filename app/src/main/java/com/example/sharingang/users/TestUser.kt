6057afe8d9c4c6ce7c7cefc50b6c771e1f4be453
lumapools <benedek.hauer@gmail.com>
2021-04-08 21:36:50 +0200

    Added TestUser as object

---
+++ app/src/main/java/com/example/sharingang/users/TestUser.kt
@@ -0,0 +1,9 @@
+package com.example.sharingang.users
+
+object TestUser {
+    val self = User(
+        id = null,
+        name = "Test User",
+        profilePicture = "https://picsum.photos/200",
+    )
+}
