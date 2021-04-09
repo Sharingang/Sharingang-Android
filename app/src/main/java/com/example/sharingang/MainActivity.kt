8f58bfb4f07a9c4d12cfbb821260a22eaee76805
Lúcás Meier <cronokirby@gmail.com>
2021-04-09 12:18:44 +0200

    Try increasing coverage through superfluous methods

--- app/src/main/java/com/example/sharingang/MainActivity.kt
+++ app/src/main/java/com/example/sharingang/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.sharingang
 
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.onNavDestinationSelected
@@ -15,19 +15,25 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    private fun getNavController(): NavController {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        return navHostFragment.navController
+    }
 
+    private fun setupNavigation(navController: NavController) {
         val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
         val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
         findViewById<Toolbar>(R.id.toolbar)
             .setupWithNavController(navController, appBarConfiguration)
         findViewById<NavigationView>(R.id.nav_view)
             .setupWithNavController(navController)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setupNavigation(getNavController())
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
