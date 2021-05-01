82586a24ef600ba2aeb6502453a1e10783d830ea
Sebastian Velez de Villa <sebastian.velezdevilla@epfl.ch>
2021-05-01 12:14:39 +0200

    Add comments

--- app/src/main/java/com/example/sharingang/utils/UIViewModel.kt
+++ app/src/main/java/com/example/sharingang/utils/UIViewModel.kt
@@ -5,10 +5,13 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class UIViewModel : ViewModel() {
+
+    // contains the spinner position containing we're ordering the items in ascending or descending position
     private val _ascendingDescendingPosition = MutableLiveData(0)
     val ascendingDescendingPosition: LiveData<Int>
         get() = _ascendingDescendingPosition
 
+    // contains the spinner position containing how we're currently ordering the items
     private val _orderByPosition = MutableLiveData(0)
     val orderByPosition: LiveData<Int>
         get() = _orderByPosition
