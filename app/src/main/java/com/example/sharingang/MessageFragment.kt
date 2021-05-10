459a47992744d454857c67de998895a88947e454
lumapools <benedek.hauer@gmail.com>
2021-05-10 15:31:37 +0200

    Message can be sent only when message != empty

--- app/src/main/java/com/example/sharingang/MessageFragment.kt
+++ app/src/main/java/com/example/sharingang/MessageFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.sharingang.databinding.FragmentMessageBinding
@@ -14,6 +15,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -42,6 +44,7 @@ class MessageFragment : Fragment() {
         partnerProfilePic = args.partnerProfilePictureUrl
         currentUser = auth.currentUser!!
         setupFields()
+        setupSendButton()
         return binding.root
     }
 
@@ -49,5 +52,32 @@ class MessageFragment : Fragment() {
         Glide.with(this).load(partnerProfilePic).into(binding.chatPartnerPic)
         binding.chatPartnerUsername.text = partnerUsername
         Log.e("xxx", "name = $partnerUsername")
+    }
+
+    private fun setupSendButton() {
+        binding.btnSend.isEnabled = false
+        binding.messageEditText.addTextChangedListener {
+            binding.btnSend.isEnabled = !it.isNullOrBlank()
+        }
+        binding.btnSend.setOnClickListener {
+            val message: String = binding.messageEditText.text.toString()
+            sendMessage(currentUser.uid, partnerId, message)
+        }
+    }
+
+    private fun sendMessage(from: String, to: String, message: String) {
+        val data = hashMapOf<String, Any>(
+            "from" to from,
+            "to" to to,
+            "message" to message,
+            "date" to Date()
+        )
+        firebaseFirestore.collection("users").document(from)
+            .collection("chats").document(to).collection("messages")
+            .add(data)
+        firebaseFirestore.collection("users").document(to)
+            .collection("chats").document(from).collection("messages")
+            .add(data)
+        binding.messageEditText.text.clear()
     }
 }
