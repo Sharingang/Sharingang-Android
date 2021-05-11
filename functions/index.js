d28b282c3d962ed65da07e999e9234b084b777d3
Samuel Bétrisey <samuel@sam.ninja>
2021-05-11 19:21:28 +0200

    Complete checkout cloud function

--- functions/index.js
+++ functions/index.js
@@ -6,41 +6,54 @@ const stripe = require("stripe")(secret_key);
 
 const admin = require('firebase-admin');
 admin.initializeApp();
+const db = admin.firestore();
+const auth = admin.auth();
 
 exports.checkout = functions.https.onCall(async (data, context) => {
     if (!context.auth) {
         throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
     }
 
-    const itemId = data.itemId;
-    console.log(itemId);
-    console.log(context.auth.uid);
+    const itemPromise = db.collection('items').doc(data.itemId).get();
 
-    // Create or retrieve the Stripe Customer object associated with your user.
-    let customer = await stripe.customers.create(); // This example just creates a new Customer every time
+    const user = await auth.getUser(context.auth.uid)
+
+    const customers = (await stripe.customers.list({ email: user.email })).data;
+    var customer;
+    if (customers.length > 0) {
+        customer = customers[0];
+    } else {
+        customer = await stripe.customers.create({
+            name: user.displayName,
+            email: user.email
+        });
+    }
 
     // Create an ephemeral key for the Customer; this allows the app to display saved payment methods and save new ones
-    const ephemeralKey = await stripe.ephemeralKeys.create(
+    const ephemeralKeyPromise = stripe.ephemeralKeys.create(
         { customer: customer.id },
         { apiVersion: '2020-08-27' }
     );
 
-    // Create a PaymentIntent with the payment amount, currency, and customer
+    const item = (await itemPromise).data();
+    // Stripe expects price in cents
+    const price = Math.round(item.price * 100);
+
     const paymentIntent = await stripe.paymentIntents.create({
-        amount: 973,
+        amount: price,
         currency: "chf",
         customer: customer.id
     });
+
+    console.log("Created payment intent", { email: user.email, customerId: customer.id, itemId: data.itemId, price: price });
 
     // Send the object keys to the client
     return {
         publishableKey: publishable_key,
         paymentIntent: paymentIntent.client_secret,
         customer: customer.id,
-        ephemeralKey: ephemeralKey.secret
+        ephemeralKey: (await ephemeralKeyPromise).secret
     }
-
-    // const writeResult = await admin.firestore().collection('messages').add({ original: original });
 });
 
 // // Create and Deploy Your First Cloud Functions
