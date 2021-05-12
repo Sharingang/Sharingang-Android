1fe9fa3b0723043b6dc66352b4050a1b90566dfb
lumapools <benedek.hauer@gmail.com>
2021-05-12 22:47:01 +0200

    Remove toString method from Chat
    It was only used for debugging and it decreases coverage.

--- app/src/main/java/com/example/sharingang/Chat.kt
+++ app/src/main/java/com/example/sharingang/Chat.kt
@@ -7,8 +7,4 @@ package com.example.sharingang
  * @param to the receiver
  * @param message the actual message
  */
-class Chat constructor(val from: String?, val to: String?, val message: String) {
-    override fun toString(): String {
-        return message
-    }
-}
+class Chat constructor(val from: String?, val to: String?, val message: String)
