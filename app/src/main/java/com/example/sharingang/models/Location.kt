67bdc1ace4a201e666e3e38729be0744dddcadab
Lúcás Meier <cronokirby@gmail.com>
2021-05-18 16:33:36 +0200

    Migrate old branch

---
+++ app/src/main/java/com/example/sharingang/models/Location.kt
@@ -0,0 +1,12 @@
+package com.example.sharingang.models
+
+/**
+ * Location represents a single point on the Earth.
+ *
+ * We use a latitude, and a longitude, both doubles. Now, doubles can be somewhat misleading,
+ * because the precision of the sensor data we receive is actually less accurate.
+ * On the other hand, it can't actually hurt to use more accurate numbers when doing
+ * calculations. We also don't perform many expensive calculations, so the use of Double
+ * isn't very onerous either.
+ */
+data class Location(val lat: Double, val lon: Double)
---
+++ app/src/main/java/com/example/sharingang/ui/activities/ARActivity.kt
@@ -0,0 +1,68 @@
+package com.example.sharingang.ui.activities
+
+import android.content.Context
+import android.hardware.SensorManager
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.navArgs
+import com.example.sharingang.R
+import com.example.sharingang.databinding.ActivityAractivityBinding
+import com.example.sharingang.models.Location
+import com.example.sharingang.shake.HeadingListener
+import com.example.sharingang.viewmodels.ARViewModel
+import com.google.android.gms.location.*
+
+/**
+ * An Activity we use for the AR interactions.
+ *
+ * We create a separate activity instead of a fragment, because we need to listen
+ * to extra sensors and other things like that, and we want to avoid cluttering up
+ * the main activity with all of that.
+ */
+class ARActivity : AppCompatActivity() {
+    // The arguments passed in when navigating to this activity
+    // This is how we get information about the item we're looking at
+    private val args: ARActivityArgs by navArgs()
+
+    // The view model holding the state of this activity
+    private val viewModel: ARViewModel by viewModels()
+
+    private lateinit var sensorManager: SensorManager
+    private lateinit var headingListener: HeadingListener
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aractivity)
+
+        val binding: ActivityAractivityBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_aractivity)
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+        viewModel.setItemLocation(Location(args.item.latitude, args.item.longitude))
+
+        setupHeadingListener()
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        setupHeadingListener()
+    }
+
+    private fun setupHeadingListener() {
+        headingListener = HeadingListener {
+            viewModel.setHeading(it)
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        headingListener.registerWith(sensorManager)
+    }
+
+    override fun onPause() {
+        sensorManager.unregisterListener(headingListener)
+        super.onPause()
+    }
+}
