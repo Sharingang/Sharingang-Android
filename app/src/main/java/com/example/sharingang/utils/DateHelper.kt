cdb2fbefe887b018532364d16fe1dd83adca28f5
hauer <benedek.hauer@epfl.ch>
2021-05-22 06:58:32 +0200

    KDoc + improve/refactor DateHelper

--- app/src/main/java/com/example/sharingang/utils/DateHelper.kt
+++ app/src/main/java/com/example/sharingang/utils/DateHelper.kt
@@ -1,48 +1,56 @@
 package com.example.sharingang.utils
 
 import java.util.*
-import java.util.concurrent.TimeUnit
 
 object DateHelper {
-    fun getDateDifferenceInDays(startDate: Date, endDate: Date): Long {
-        return TimeUnit.DAYS.convert(endDate.time - startDate.time,
-            TimeUnit.MILLISECONDS)
-    }
 
+    /**
+     * formats a date for message timestamps
+     *
+     * @param date the date to format
+     * @return the formatted date
+     */
     fun formatMessageDate(date: Date): String {
         val currentDate = Date()
         val today = Triple(currentDate.year, currentDate.month, currentDate.day)
         val messageTime = Triple(date.year, date.month, date.day)
         val monthStr = getMonthName(date.month)
         return (
-                when {
-                    today == messageTime -> "Today, ${date.hours}:${
-                        if(date.minutes < 10) "0" + "${date.minutes}"
-                        else date.minutes
-                    }"
-                    today.first == messageTime.first -> "$monthStr ${date.day}"
-                    else -> "$monthStr ${date.day}, ${date.year}"
-                }
-                )
+            when {
+                today == messageTime -> "Today, ${date.hours}:${
+                    date.minutes.toString().padStart(2, '0')
+                }"
+                today.first == messageTime.first -> "$monthStr ${date.day}"
+                else -> "$monthStr ${date.day}, ${date.year}"
+            }
+        )
     }
+
+    /**
+     * get the name of the month based on its integer value in real life (i.e. 1 is equivalent
+     * to January, not February)
+     *
+     * @param monthNumber the number of the month we want to get the name of
+     * @return the name of the month
+     */
     private fun getMonthName(monthNumber: Int): String {
         return (
-                when(monthNumber) {
-                    1 -> "Jan."
-                    2 -> "Feb."
-                    3 -> "Mar."
-                    4 -> "Apr."
-                    5 -> "May."
-                    6 -> "Jun."
-                    7 -> "Jul."
-                    8 -> "Aug."
-                    9 -> "Sep."
-                    10 -> "Oct."
-                    11 -> "Nov."
-                    12 -> "Dec."
-                    else -> ""
-                }
-                )
+            when(monthNumber) {
+                1 -> "Jan."
+                2 -> "Feb."
+                3 -> "Mar."
+                4 -> "Apr."
+                5 -> "May."
+                6 -> "Jun."
+                7 -> "Jul."
+                8 -> "Aug."
+                9 -> "Sep."
+                10 -> "Oct."
+                11 -> "Nov."
+                12 -> "Dec."
+                else -> ""
+            }
+        )
     }
 
 }
