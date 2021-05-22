572be740afd68c88502f1faaf290e10681296cd2
hauer <benedek.hauer@epfl.ch>
2021-05-22 05:15:18 +0200

    Add difference formatting

--- app/src/main/java/com/example/sharingang/utils/DateHelper.kt
+++ app/src/main/java/com/example/sharingang/utils/DateHelper.kt
@@ -4,9 +4,81 @@ import java.util.*
 import java.util.concurrent.TimeUnit
 
 object DateHelper {
-    fun getDateDifferenceInDays(startDate: Date, endDate: Date): Long {
-        return TimeUnit.DAYS.convert(endDate.time - startDate.time,
+
+    enum class Measure {
+        SECONDS,
+        MINUTES,
+        HOURS,
+        DAYS,
+        WEEKS,
+        MONTHS,
+        YEARS
+    }
+
+    /*
+     * Note: It is true that days can differ between years and months. Here, we are only
+     * concerned by an estimation of the time between two dates, which is why this very simplified
+     * implementation will do (used for the last time an item was updated for example, which does
+     * not require a very precise measure of time.)
+     */
+    private const val SECS_PER_MINUTE = 60L
+    private const val SECS_PER_HOUR = 3600L
+    private const val SECS_PER_DAY = 86400L
+    private const val DAYS_PER_WEEK = 7L
+    private const val DAYS_PER_MONTH = 30L
+    private const val DAYS_PER_YEAR = 365L
+
+
+    fun getDateDifferenceString(startDate: Date, endDate: Date): String {
+        val endTime = endDate.time
+        val startTime = startDate.time
+        val diffInSeconds = TimeUnit.SECONDS.convert(endTime - startTime,
             TimeUnit.MILLISECONDS)
+        val lessThanADay = diffInSeconds < SECS_PER_DAY
+        val accurateDiff =
+            if(lessThanADay) getAccurateDiffFromSeconds(diffInSeconds)
+            else {
+                val diffInDays =
+                    TimeUnit.DAYS.convert(endTime - startTime, TimeUnit.MILLISECONDS)
+                getAccurateDiffFromDays(diffInDays)
+            }
+        return getFormattedDifference(accurateDiff)
+    }
+
+    private fun getAccurateDiffFromSeconds(diffInSeconds: Long): Pair<Long, Measure> {
+        return (
+            when {
+                diffInSeconds < SECS_PER_MINUTE -> Pair(diffInSeconds, Measure.SECONDS)
+                diffInSeconds < SECS_PER_HOUR -> Pair(diffInSeconds / SECS_PER_MINUTE, Measure.MINUTES)
+                else -> Pair(diffInSeconds / SECS_PER_DAY, Measure.HOURS)
+            }
+        )
+    }
+
+    private fun getAccurateDiffFromDays(diffInDays: Long): Pair<Long, Measure> {
+        return (
+            when {
+                diffInDays < DAYS_PER_WEEK -> Pair(diffInDays, Measure.DAYS)
+                diffInDays < DAYS_PER_MONTH -> Pair(diffInDays / DAYS_PER_WEEK, Measure.WEEKS)
+                diffInDays < DAYS_PER_YEAR -> Pair(diffInDays / DAYS_PER_MONTH, Measure.MONTHS)
+                else -> Pair(diffInDays / DAYS_PER_YEAR, Measure.YEARS)
+            }
+        )
+    }
+
+    private fun getFormattedDifference(difference: Pair<Long, Measure>): String {
+        val diffNumber = difference.first
+        val measureStr =
+            when(difference.second) {
+                Measure.SECONDS -> if(diffNumber == 1L) "second" else "seconds"
+                Measure.MINUTES -> if(diffNumber == 1L) "minute" else "minutes"
+                Measure.HOURS -> if(diffNumber == 1L) "hour" else "hours"
+                Measure.DAYS -> if(diffNumber == 1L) "day" else "days"
+                Measure.WEEKS -> if(diffNumber == 1L) "week" else "weeks"
+                Measure.MONTHS -> if(diffNumber == 1L) "month" else "months"
+                Measure.YEARS -> if(diffNumber == 1L) "year" else "years"
+            }
+        return "$diffNumber $measureStr"
     }
 
 }
