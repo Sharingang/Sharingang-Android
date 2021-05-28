aaf54d43e78b5c965b55f4186de7b3bb368f7e83
lumapools <benedek.hauer@gmail.com>
2021-05-28 22:07:14 +0200

    Create blocked users fragment

---
+++ app/src/main/java/com/example/sharingang/ui/fragments/BlockedUsersFragment.kt
@@ -0,0 +1,19 @@
+package com.example.sharingang.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.sharingang.R
+
+class BlockedUsersFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blocked_users, container, false)
+    }
+}
